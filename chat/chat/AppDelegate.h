51ff50f57a9c398bbe0255871768c3f853539f76
YJY <yujinyi1125@163.com>
2016-09-29 20:11:38 +0800

    初始化项目

---
+++ chat/chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  chat
+//
+//  Created by yjy on 16/9/29.
+//  Copyright © 2016年 jjyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ chat/chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  chat
+//
+//  Created by yjy on 16/9/29.
+//  Copyright © 2016年 jjyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
